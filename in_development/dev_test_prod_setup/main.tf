cc86b7f9f02e8163a6407a3a668eefbb187ffc35
Rui Nogueira <rui.nogueira@sap.com>
2023-06-26 08:05:03 +0200

    add creation of cf envs per stage

--- in_development/dev_test_prod_setup/main.tf
+++ in_development/dev_test_prod_setup/main.tf
@@ -12,26 +12,26 @@ resource "btp_directory" "landscapes" {
 ###
 # Creation of subaccounts
 ###
-resource "btp_subaccount" "subaccount" {
+resource "btp_subaccount" "all" {
 
   for_each = var.project
     name      = "${each.value.stage} - ${var.org_name}: ${var.department_name}"
     subdomain = lower(replace("${each.value.stage}-${var.org_name}-${var.department_name}", " ", "-"))
-    region    = var.region
+    region    = lower(var.region)
     #labels    = [{"costcenter": ["${var.costcenter}"]} ]
+
 }
 
-/*
 ###
 # Creation of Cloud Foundry environment
 ###
 module "cloudfoundry_environment" {
   source = "./modules/envinstance-cloudfoundry/"
 
-  for_each = {}
-    subaccount_id         = btp_subaccount.subaccount.id
-    instance_name         = lower(replace("${each.value.stage}_${var.org_name}_${var.department_name}", " ", "_"))
-    cloudfoundry_org_name = lower(replace("${each.value.stage}_${var.org_name}_${var.department_name}", " ", "_"))
+  for_each = btp_subaccount.all
+    subaccount_id         = each.value.id
+    instance_name         = substr(replace(lower(replace("${each.value.name}", "/\\W|_|\\s/" , "_")), "__", "_"), 0, 32)
+    cloudfoundry_org_name = replace(lower(replace("${each.value.name}", "/\\W|_|\\s/" , "_")), "__", "_")
 }
 
 
@@ -39,10 +39,11 @@ module "cloudfoundry_environment" {
 # Add entitlements to each subaccount
 ###
 resource "btp_subaccount_entitlement" "all_entitlements" {
+
   subaccount_id = btp_subaccount.subaccount.id
 
   for_each = var.entitlements
     service_name  = each.value.service_name
     plan_name     = each.value.plan_name
 }
-*/
+
