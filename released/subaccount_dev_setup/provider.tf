90a805ba61d3af22fdd506bf7f86e0796f821cc8
Christian Volk <christian.volk@sap.com>
2023-06-28 09:09:40 +0200

    deactivate provider block

--- released/subaccount_dev_setup/provider.tf
+++ released/subaccount_dev_setup/provider.tf
@@ -10,6 +10,6 @@ terraform {
 
 # Please checkout documentation on how best to authenticate against SAP BTP
 # via the Terraform provider for SAP BTP
-provider "btp" {
-  globalaccount = "yourglobalaccount_subdomain_id"
-}
+#provider "btp" {
+#  globalaccount = "yourglobalaccount_subdomain_id"
+#}
