3d75b928e26419a8b8c1bb8dd5b0d9c6911e140c
Rui Nogueira <rui.nogueira@sap.com>
2023-07-26 15:20:28 +0200

    fix provider versions + add gh action for uc

--- released/uc_multi_provider_setup/provider.tf
+++ released/uc_multi_provider_setup/provider.tf
@@ -3,11 +3,11 @@ terraform {
   required_providers {
     btp = {
       source  = "sap/btp"
-      version = "0.1.0-beta1"
+      version = "0.2.0-beta2"
     }
     cloudfoundry = {
       source  = "cloudfoundry-community/cloudfoundry"
-      version = "~> 0.50"
+      version = "0.50.8"
     }
   }
 }
