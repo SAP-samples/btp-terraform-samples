503c93fd6c5b72813edaa41b0970b13e84e5b7d6
Rui Nogueira <rui.nogueira@sap.com>
2023-07-28 11:34:53 +0200

    add module

--- released/modules/sap_build_apps/standard/variables.tf
+++ released/modules/sap_build_apps/standard/variables.tf
@@ -3,20 +3,11 @@ variable "subaccount_id" {
   description = "The subaccount id"
 }
 
-variable "for_sap_build_apps_roles_to_create"{
-  type        = list(string)
-  description = "Defines the colleagues who are added to each subaccount as emergency administrators."
-  default     = ["BuildAppsAdmin", "BuildAppsDeveloper", "RegistryAdmin", "RegistryDeveloper"]
+variable "entitlements" {
+  description = "List of entitlements for a BTP subaccount"
+    type = list(object({
+      name = string
+      plan = string
+      type = string
+  }))
 }
-/*
-variable "custom_idp" {
-  type        = string
-  description = "Defines the custom IDP to be used for the subaccount"
-  default = "terraformint"
-
-  validation {
-    condition = can(regex("^[a-z-]", var.custom_idp))
-    error_message = "Please enter a valid entry for the custom-idp of the subaccount."
-  }
-}
-*/
