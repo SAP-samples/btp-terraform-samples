d39fc823d679e55277b1a3f273bf5c4b1556ac25
Christian Lechner <lechnerc77@users.noreply.github.com>
2023-08-24 13:26:37 +0200

    chore: version update of SAP BTP and CF provider

--- released/modules/cloudfoundry-service-instance/provider.tf
+++ released/modules/cloudfoundry-service-instance/provider.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     cloudfoundry = {
       source  = "cloudfoundry-community/cloudfoundry"
-      version = "0.50.8"
+      version = "0.51.3"
     }
   }
 }
--- released/uc_multi_provider_setup/provider.tf
+++ released/uc_multi_provider_setup/provider.tf
@@ -3,11 +3,11 @@ terraform {
   required_providers {
     btp = {
       source  = "sap/btp"
-      version = "0.2.0-beta2"
+      version = "0.3.0-beta1"
     }
     cloudfoundry = {
       source  = "cloudfoundry-community/cloudfoundry"
-      version = "0.50.8"
+      version = "0.51.3"
     }
   }
 }
