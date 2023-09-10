87be52fdc0f4afbd3db181b0c7bd0d18bc49d05f
Mahesh kumar Palavalli <p.maheshkumarreddy@gmail.com>
2023-09-10 15:54:34 +0530

    uc: events to business action

---
+++ released/uc_events-to-business-actions/terraform.tfvars
@@ -0,0 +1,16 @@
+#################################
+# Project specific configuration
+#################################
+# Your global account subdomain
+globalaccount        = "ticoo"
+region               = "us10"
+subaccount_name      = "UC - Events to Business Actions"
+cf_environment_label = "cf-us10"
+cf_space_name        = "dev"
+
+subaccount_admins         = ["shanthakumar.krishnaswamy@sap.com"]
+subaccount_service_admins = ["m.palavalli@sap.com","shanthakumar.krishnaswamy@sap.com"]
+
+cf_space_managers   = ["m.palavalli@sap.com", "shanthakumar.krishnaswamy@sap.com"]
+cf_space_developers = ["m.palavalli@sap.com"]
+cf_space_auditors   = ["m.palavalli@sap.com"]
---
+++ released/uc_events-to-business-actions/variables.tf
@@ -0,0 +1,142 @@
+######################################################################
+# Customer account setup
+######################################################################
+# subaccount
+variable "globalaccount" {
+  type        = string
+  description = "The globalaccount subdomain."
+  default     = "yourglobalaccount"
+}
+# subaccount
+variable "subaccount_name" {
+  type        = string
+  description = "The subaccount name."
+  default     = "UC - Events to Business Actions"
+}
+# Region
+variable "region" {
+  type        = string
+  description = "The region where the project account shall be created in."
+  default     = "us10"
+}
+# Cloudfoundry environment label
+variable "cf_environment_label" {
+  type        = string
+  description = "The Cloudfoundry environment label"
+  default     = "cf-us10"
+}
+
+# Cloudfoundry space name
+variable "cf_space_name" {
+  type        = string
+  description = "The Cloudfoundry space name"
+  default     = "dev"
+}
+
+# hana password
+variable "hana_cloud_system_password" {
+  type        = string
+  description = "The system password for the hana_cloud service instance."
+  default     = "Abcd1234"
+}
+
+# CLI server
+variable "cli_server_url" {
+  type        = string
+  description = "The BTP CLI server URL."
+  default     = "https://cpcli.cf.eu10.hana.ondemand.com"
+}
+
+variable "subaccount_admins" {
+  type        = list(string)
+  description = "Defines the colleagues who are added to each subaccount as subaccount administrators."
+  default     = ["jane.doe@test.com", "john.doe@test.com"]
+}
+
+variable "subaccount_service_admins" {
+  type        = list(string)
+  description = "Defines the colleagues who are added to each subaccount as subaccount service administrators."
+  default     = ["jane.doe@test.com", "john.doe@test.com"]
+}
+
+variable "cf_space_managers" {
+  type        = list(string)
+  description = "Defines the colleagues who are Cloudfoundry space managers"
+  default     = ["jane.doe@test.com", "john.doe@test.com"]
+}
+
+variable "cf_space_developers" {
+  type        = list(string)
+  description = "Defines the colleagues who are Cloudfoundry space developers"
+  default     = ["jane.doe@test.com", "john.doe@test.com"]
+}
+
+variable "cf_space_auditors" {
+  type        = list(string)
+  description = "Defines the colleagues who are Cloudfoundry space auditors"
+  default     = ["jane.doe@test.com", "john.doe@test.com"]
+}
+
+###
+# Entitlements
+###
+variable "entitlements" {
+  type = list(object({
+    service_name = string
+    plan_name    = string
+    type         = string
+  }))
+  description = "The list of entitlements that shall be added to the subaccount."
+  default = [
+    {
+      service_name = "connectivity"
+      plan_name    = "lite",
+      type         = "service"
+    },
+    {
+      service_name = "destination"
+      plan_name    = "lite",
+      type         = "service"
+    },
+    {
+      service_name = "html5-apps-repo"
+      plan_name    = "app-host",
+      type         = "service"
+    },
+    {
+      service_name = "sapappstudio"
+      plan_name    = "standard-edition",
+      type         = "app"
+    },
+    {
+      service_name = "enterprise-messaging"
+      plan_name    = "default",
+      type         = "service"
+    },
+    {
+      service_name = "enterprise-messaging-hub"
+      plan_name    = "standard",
+      type         = "app"
+    },
+    {
+      service_name = "privatelink"
+      plan_name    = "standard",
+      type         = "service"
+    },
+    {
+      service_name = "xsuaa"
+      plan_name    = "application",
+      type         = "service"
+    },
+    {
+      service_name = "hana"
+      plan_name    = "hdi-shared",
+      type         = "service"
+    },
+    {
+      service_name = "hana-cloud"
+      plan_name    = "hana",
+      type         = "service"
+    }
+  ]
+}
