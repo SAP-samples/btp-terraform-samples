dfc94c038af56e2e2e80c4b8cc274f6ba41fbdcd
Marcel Schork <marcel.schork@mail.schwarz>
2023-09-15 15:58:03 +0200

    fix: remove unsed variables and provider declaration

--- released/modules/envinstance-cloudfoundry/provider.tf
+++ released/modules/envinstance-cloudfoundry/provider.tf
@@ -4,9 +4,5 @@ terraform {
       source  = "sap/btp"
       version = "0.4.0-beta1"
     }
-    cloudfoundry = {
-      source  = "cloudfoundry-community/cloudfoundry"
-      version = "0.51.3"
-    }
   }
 }
