09c6fdd6ded936e5edd486d2505fc3422951236a
Rui Nogueira <rui1610@users.noreply.github.com>
2023-10-04 10:50:37 +0200

    Update main.tf
    Updated look-and-feel and linked to new places for modules.
    
    Please check the code with the new link to the modules.

--- released/uc_build_process_automation/main.tf
+++ released/uc_build_process_automation/main.tf
@@ -1,24 +1,24 @@
-###############################################################################################
+# ------------------------------------------------------------------------------------------------------
 # Setup of names in accordance to naming convention
-###############################################################################################
+# ------------------------------------------------------------------------------------------------------
 locals {
   random_uuid               = uuid()
   project_subaccount_domain = "teched23-tf-sap-ms-${local.random_uuid}"
   project_subaccount_cf_org = substr(replace("${local.project_subaccount_domain}", "-", ""), 0, 32)
 }
 
-###############################################################################################
+# ------------------------------------------------------------------------------------------------------
 # Creation of subaccount
-###############################################################################################
+# ------------------------------------------------------------------------------------------------------
 resource "btp_subaccount" "project" {
   name      = var.subaccount_name
   subdomain = local.project_subaccount_domain
   region    = lower(var.region)
 }
 
-###############################################################################################
+# ------------------------------------------------------------------------------------------------------
 # Assignment of users as sub account administrators
-###############################################################################################
+# ------------------------------------------------------------------------------------------------------
 resource "btp_subaccount_role_collection_assignment" "subaccount-admins" {
   for_each             = toset("${var.subaccount_admins}")
   subaccount_id        = btp_subaccount.project.id
@@ -26,9 +26,9 @@ resource "btp_subaccount_role_collection_assignment" "subaccount-admins" {
   user_name            = each.value
 }
 
-###############################################################################################
+# ------------------------------------------------------------------------------------------------------
 # Assignment of users as sub account service administrators
-###############################################################################################
+# ------------------------------------------------------------------------------------------------------
 resource "btp_subaccount_role_collection_assignment" "subaccount-service-admins" {
   for_each             = toset("${var.subaccount_service_admins}")
   subaccount_id        = btp_subaccount.project.id
@@ -36,22 +36,22 @@ resource "btp_subaccount_role_collection_assignment" "subaccount-service-admins"
   user_name            = each.value
 }
 
-######################################################################
+# ------------------------------------------------------------------------------------------------------
 # Creation of Cloud Foundry environment
-######################################################################
+# ------------------------------------------------------------------------------------------------------
 module "cloudfoundry_environment" {
-  source                = "../modules/envinstance-cloudfoundry/"
+  source                = "../../modules/environment/coudfoundry/envinstance-cf/"
   subaccount_id         = btp_subaccount.project.id
   instance_name         = local.project_subaccount_cf_org
   plan_name             = "standard"
   cloudfoundry_org_name = local.project_subaccount_cf_org
 }
 
-######################################################################
+# ------------------------------------------------------------------------------------------------------
 # Creation of Cloud Foundry space
-######################################################################
+# ------------------------------------------------------------------------------------------------------
 module "cloudfoundry_space" {
-  source              = "../modules/cloudfoundry-space/"
+  source              = "../../modules/environment/coudfoundry/space-cf/"
   cf_org_id           = module.cloudfoundry_environment.org_id
   name                = var.cf_space_name
   cf_space_managers   = var.cf_space_managers
@@ -59,17 +59,17 @@ module "cloudfoundry_space" {
   cf_space_auditors   = var.cf_space_auditors
 }
 
-######################################################################
+# ------------------------------------------------------------------------------------------------------
 # Add "sleep" resource for generic purposes
-######################################################################
+# ------------------------------------------------------------------------------------------------------
 resource "time_sleep" "wait_a_few_seconds" {
   create_duration = "30s"
 }
 
 
-######################################################################
+# ------------------------------------------------------------------------------------------------------
 # Create service instance - SAP Build Process Automation service
-######################################################################
+# ------------------------------------------------------------------------------------------------------
 resource "btp_subaccount_entitlement" "bpa" {
   depends_on = [time_sleep.wait_a_few_seconds]
   subaccount_id = btp_subaccount.project.id
