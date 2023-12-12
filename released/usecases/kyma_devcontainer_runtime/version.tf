36c116aa3618ee75107ddbba65989855b4fd18e9
Christian Volk <christian.volk@sap.com>
2023-12-12 17:32:47 +0100

    Update version.tf

--- released/usecases/kyma_devcontainer_runtime/version.tf
+++ released/usecases/kyma_devcontainer_runtime/version.tf
@@ -6,3 +6,10 @@ terraform {
     }
   }
 }
+
+# Please checkout documentation on how best to authenticate against SAP BTP
+# via the Terraform provider for SAP BTP
+
+# provider "btp" {
+#   globalaccount = "<YOUR GLOBALACCOUNT SUBDOMAIN>"
+# }
