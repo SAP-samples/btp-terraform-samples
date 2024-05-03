87dea3829ef930f415f28db88f25066b042028c8
Rui Nogueira <rui1610@users.noreply.github.com>
2024-05-03 12:46:59 +0200

    update tf provider to 1.1.0 (#140)

--- released/dsag/betriebstag2024/solution/provider.tf
+++ released/dsag/betriebstag2024/solution/provider.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     btp = {
       source  = "sap/btp"
-      version = "1.2.0"
+      version = "1.1.0"
     }
     cloudfoundry = {
       source  = "cloudfoundry-community/cloudfoundry"
