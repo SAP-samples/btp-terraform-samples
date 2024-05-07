33e2600e8afdf9129cc73fe7f6ae5e2b4c28af25
Rui Nogueira <rui.nogueira@sap.com>
2024-05-07 14:22:13 +0000

    add role assignments

--- released/usecases/services_apps/sap_build_code/step2/main.tf
+++ released/usecases/services_apps/sap_build_code/step2/main.tf
@@ -1,6 +1,61 @@
+# ------------------------------------------------------------------------------------------------------
+# Create the Cloud Foundry space
+# ------------------------------------------------------------------------------------------------------
 resource "cloudfoundry_space" "space" {
-  name      = "space"
-  org       = "ca721b24-e24d-4171-83e1-1ef6bd836b38"
-  allow_ssh = "true"
-  labels    = { test : "pass", purpose : "prod" }
+  name = "space"
+  org  = var.cf_org_id
+}
+
+# ------------------------------------------------------------------------------------------------------
+# Prepare the list of admins and roles for the CF Org 
+# ------------------------------------------------------------------------------------------------------
+locals {
+  role_mapping_admins_org = distinct(flatten([
+    for admin in var.admins : [
+      for role in var.roles_cf_org : {
+        user_name = admin
+        role_name = role
+      }
+    ]
+    ]
+  ))
+}
+locals {
+  role_mapping_admins_space = distinct(flatten([
+    for admin in var.admins : [
+      for role in var.roles_cf_space : {
+        user_name = admin
+        role_name = role
+      }
+    ]
+    ]
+  ))
+}
+
+
+
+# ------------------------------------------------------------------------------------------------------
+# Assign CF Org roles to the admin users
+# ------------------------------------------------------------------------------------------------------
+# Define Org Manager role
+resource "cloudfoundry_org_role" "all_org_roles" {
+
+  for_each = { for entry in local.role_mapping_admins_org : "${entry.user_name}.${entry.role_name}" => entry  }
+
+  username = each.value.user_name
+  type     = each.value.role_name
+  org      = var.cf_org_id
+}
+
+# ------------------------------------------------------------------------------------------------------
+# Assign CF space roles to the admin users
+# ------------------------------------------------------------------------------------------------------
+# Define Space Manager role
+resource "cloudfoundry_space_role" "my_role" {
+  for_each = { for entry in local.role_mapping_admins_space : "${entry.user_name}.${entry.role_name}" => entry  }
+
+  username = each.value.user_name
+  type     = each.value.role_name
+  space      = cloudfoundry_space.space.id
+  depends_on = [cloudfoundry_org_role.all_org_roles]
 }
--- released/usecases/services_apps/sap_build_code/step2/variables.tf
+++ released/usecases/services_apps/sap_build_code/step2/variables.tf
@@ -14,3 +14,38 @@ variable "cf_org_name" {
   description = "The Cloud Foundry Org name from the Cloud Foundry environment instance."
  
 }
+
+variable "admins" {
+  type        = list(string)
+  description = "Defines the colleagues who are added to each cf org."
+
+  # add validation to check if admins contains a list of valid email addresses
+  validation {
+    condition     = length([for email in var.admins : can(regex("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$", email))]) == length(var.admins)
+    error_message = "Please enter a valid email address for the admins."
+  }
+}
+
+# Define roles for a CF user
+variable "roles_cf_org" {
+  type        = list(string)
+  description = "Defines the list of roles to be assigned to the users in a CF Org."
+  default = [
+    "organization_user",
+    "organization_manager",
+    "organization_auditor",
+    "organization_billing_manager"
+  ]
+}
+
+# Define roles for a user in the CF space
+variable "roles_cf_space" {
+  type        = list(string)
+  description = "Defines the list of roles to be assigned to the users in a CF Org."
+  default = [
+    "space_developer",
+    "space_supporter",
+    "space_manager",
+    "space_auditor"
+  ]
+}
