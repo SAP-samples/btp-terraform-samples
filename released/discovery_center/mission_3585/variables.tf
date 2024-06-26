7881a5c0e608136621842622da22dca93bfe4f19
Rui Nogueira <rui1610@users.noreply.github.com>
2024-06-26 16:25:03 +0200

    Update variables.tf
    Removing unnecessary variables (e.g. the ones for creating a cf space, as the script didn't create a cf space at all.

--- released/discovery_center/mission_3585/variables.tf
+++ released/discovery_center/mission_3585/variables.tf
@@ -6,102 +6,106 @@ variable "globalaccount" {
 variable "subaccount_name" {
   type        = string
   description = "The subaccount name."
-  default     = "My SAP Build Apps subaccount"
+  default     = "My SAP DC mission subaccount."
 }
 
-variable "subaccount_id" {
+variable "cli_server_url" {
   type        = string
-  description = "The subaccount ID."
-  default     = ""
-}
-
-
-variable "custom_idp" {
-  type        = string
-  description = "Defines the custom IDP to be used for the subaccount"
-  default     = ""
-}
-
-variable "origin" {
-  type        = string
-  description = "The IAS of the user configuration for the cloudfoundry environment"
-  default     = "sap.ids"
+  description = "The BTP CLI server URL."
+  default     = "https://cli.btp.cloud.sap"
 }
 
 variable "region" {
   type        = string
-  description = "The region where the sub account shall be created in."
+  description = "The region where the subaccount shall be created in."
   default     = "us10"
 }
 
-variable "org" {
-  type        = string
-  description = "Your SAP BTP org e.g. department"
-  default     = "org"
+variable "subaccount_admins" {
+  type        = list(string)
+  description = "Defines the colleagues who are added to each subaccount as emergency administrators."
+
+  # add validation to check if admins contains a list of valid email addresses
+  validation {
+    condition     = length([for email in var.subaccount_admins : can(regex("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$", email))]) == length(var.subaccount_admins)
+    error_message = "Please enter a valid email address for the subaccount admins."
+  }
 }
 
-variable "environment_label" {
+variable "cf_org_admins" {
+  type        = list(string)
+  description = "List of users to set as Cloudfoundry org administrators."
+
+  # add validation to check if admins contains a list of valid email addresses
+  validation {
+    condition     = length([for email in var.cf_org_admins : can(regex("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$", email))]) == length(var.cf_org_admins)
+    error_message = "Please enter a valid email address for the CF Org admins."
+  }
+}
+
+variable "launchpad_admins" {
+  type        = list(string)
+  description = "Defines the colleagues who are Launchpad Admins."
+
+  # add validation to check if admins contains a list of valid email addresses
+  validation {
+    condition     = length([for email in var.launchpad_admins : can(regex("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$", email))]) == length(var.launchpad_admins)
+    error_message = "Please enter a valid email address."
+  }
+}
+
+variable "bas_admins" {
+  type        = list(string)
+  description = "Defines the colleagues who are admins for SAP Business Application Studio."
+
+  # add validation to check if admins contains a list of valid email addresses
+  validation {
+    condition     = length([for email in var.bas_admins : can(regex("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$", email))]) == length(var.bas_admins)
+    error_message = "Please enter a valid email address."
+  }
+}
+
+variable "bas_developers" {
+  type        = list(string)
+  description = "Defines the colleagues who are developers for SAP Business Application Studio."
+
+  # add validation to check if admins contains a list of valid email addresses
+  validation {
+    condition     = length([for email in var.bas_developers : can(regex("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$", email))]) == length(var.bas_developers)
+    error_message = "Please enter a valid email address."
+  }
+}
+
+variable "cicd_developers" {
+  type        = list(string)
+  description = "Defines the colleagues who are developers for the CI/CD service."
+
+  # add validation to check if admins contains a list of valid email addresses
+  validation {
+    condition     = length([for email in var.cicd_developers : can(regex("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$", email))]) == length(var.cicd_developers)
+    error_message = "Please enter a valid email address."
+  }
+}
+
+variable "cicd_admins" {
+  type        = list(string)
+  description = "Defines the colleagues who are developers for the CI/CD service."
+
+  # add validation to check if admins contains a list of valid email addresses
+  validation {
+    condition     = length([for email in var.cicd_admins : can(regex("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$", email))]) == length(var.cicd_admins)
+    error_message = "Please enter a valid email address."
+  }
+}
+
+variable "cf_environment_label" {
   type        = string
   description = "In case there are multiple environments available for a subaccount, you can use this label to choose with which one you want to go. If nothing is given, we take by default the first available."
   default     = ""
 }
 
-variable "build_workzone_service_plan" {
+variable "custom_idp" {
   type        = string
-  description = "The plan for build_workzone subscription"
-  default     = "free"
-  validation {
-    condition     = contains(["free", "standard"], var.build_workzone_service_plan)
-    error_message = "Invalid value for build_workzone_service_plan. Only 'free' and 'standard' are allowed."
-  }
-}
-
-variable "bas_service_plan" {
-  type        = string
-  description = "The plan for SAP Business Application Studio subscription"
-  default     = "free"
-  validation {
-    condition     = contains(["free", "standard-edition"], var.bas_service_plan)
-    error_message = "Invalid value for SAP Business Application Studion. Only 'free' and 'standard-edition' are allowed."
-  }
-}
-
-variable "cicd_service_plan" {
-  type        = string
-  description = "The plan for Continous Integraion & Delivery subscription"
-  default     = "free"
-  validation {
-    condition     = contains(["free", "default"], var.cicd_service_plan)
-    error_message = "Invalid value for Continous Integraion & Delivery. Only 'free' and 'default' are allowed."
-  }
-}
-
-variable "admins" {
-  type        = list(string)
-  description = "Defines the colleagues who are added to each subaccount as emergency administrators."
-}
-
-variable "developers" {
-  type        = list(string)
-  description = "Defines the colleagues who are added to each subaccount as developers."
-}
-
-variable "btp_username" {
-  type        = string
-  description = "SAP BTP user name"
-}
-
-
-variable "btp_password" {
-  type        = string
-  description = "Password for SAP BTP user"
-  sensitive   = true
-}
-variable "cf_url" {
-  type    = string
-  default = "URL to Cloud Foundry landscape"
-}
-variable "cf_space" {
-  type        = string
-  description = "Name of the cloud foundry space"
+  description = "The custom identity provider for the subaccount."
+  default     = "sap.ids"
 }
