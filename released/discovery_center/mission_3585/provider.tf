28ab0db6dd9ba688d6fa4da0e76492a3c3a54761
Oliver Stiefbold <oliver.stiefbold@sap.com>
2024-06-26 15:01:41 +0200

    fix terraform format

--- released/discovery_center/mission_3585/provider.tf
+++ released/discovery_center/mission_3585/provider.tf
@@ -1,31 +1,3 @@
-terraform {
-  required_providers {
-    btp = {
-      source  = "sap/btp"
-      version = "~> 1.3.0"
-    }
-    cloudfoundry = {
-      source  = "SAP/cloudfoundry"
-      version = "0.2.1-beta"
-    }
-  }
-}
-
-# Please checkout documentation on how best to authenticate against SAP BTP
-# via the Terraform provider for SAP BTP
-provider "btp" {
-  globalaccount = var.globalaccount
-  username      = var.btp_username
-  password      = var.btp_password
-
-}
-
-provider "cloudfoundry" {
-  api_url  = var.cf_url
-  user     = var.btp_username
-  password = var.btp_password
-}
-=======
 
 terraform {
   required_providers {
@@ -54,4 +26,3 @@ provider "cloudfoundry" {
   user     = var.btp_username
   password = var.btp_password
 }
->>>>>>> f241fa4c7dbac5a20980a9a27b59698656e87cc5
