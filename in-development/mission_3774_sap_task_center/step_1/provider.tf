8bdd235dc6962fc37128ddf66c957bb9beb03642
Fabian Lehmann <fabian.lehmann02@sap.com>
2024-06-26 09:33:55 +0200

    removing cf provider

--- in-development/mission_3774_sap_task_center/step_1/provider.tf
+++ in-development/mission_3774_sap_task_center/step_1/provider.tf
@@ -6,16 +6,9 @@ terraform {
     btp = {
       source = "sap/btp"
     }
-    cloudfoundry = {
-      source = "SAP/cloudfoundry"
-      version = "0.2.1-beta"
-    }
   }
 }
 provider "btp" {
   globalaccount  = var.globalaccount
   cli_server_url = var.cli_server_url
-}
-provider "cloudfoundry" {
-  api_url  = "https://api.cf.us10.hana.ondemand.com/"
 }
