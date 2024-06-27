d54e152e10e93bc42bd5d463320dff9d1fd4075c
Rui Nogueira <rui.nogueira@sap.com>
2024-06-27 07:09:19 +0000

    initial commit

---
+++ released/discovery_center/mission_4108/main.tf
@@ -0,0 +1,81 @@
+# ------------------------------------------------------------------------------------------------------
+# Subaccount setup for DC mission 4108
+# ------------------------------------------------------------------------------------------------------
+# Setup subaccount domain (to ensure uniqueness in BTP global account)
+resource "random_uuid" "subaccount_domain_suffix" {}
+
+locals {
+  random_uuid               = random_uuid.subaccount_domain_suffix.result
+  project_subaccount_domain = lower(replace("dcmission-4108-${local.random_uuid}", "_", "-"))
+}
+
+# ------------------------------------------------------------------------------------------------------
+# Creation of subaccount
+# ------------------------------------------------------------------------------------------------------
+resource "btp_subaccount" "dc_mission" {
+  name      = var.subaccount_name
+  subdomain = local.project_subaccount_domain
+  region    = lower(var.region)
+}
+
+
+# ------------------------------------------------------------------------------------------------------
+# SERVICES
+# ------------------------------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------------------------------
+# Setup data-analytics-osb (not running in CF environment)
+# ------------------------------------------------------------------------------------------------------
+# Entitle 
+resource "btp_subaccount_entitlement" "datasphere" {
+  subaccount_id = btp_subaccount.dc_mission.id
+  service_name  = "data-analytics-osb"
+  plan_name     = "standard"
+}
+# Get serviceplan_id for data-analytics-osb with plan_name "standard"
+data "btp_subaccount_service_plan" "datasphere" {
+  subaccount_id = btp_subaccount.dc_mission.id
+  offering_name = "data-analytics-osb"
+  name          = "standard"
+  depends_on    = [btp_subaccount_entitlement.datasphere]
+}
+
+# Create service instance
+resource "btp_subaccount_service_instance" "datasphere" {
+  subaccount_id  = btp_subaccount.dc_mission.id
+  serviceplan_id = data.btp_subaccount_service_plan.datasphere.id
+  name           = "standard_datasphere-service"
+  parameters = jsonencode(
+    {
+      "first_name" : "${var.qas_datasphere_first_name}",
+      "last_name"  : "${var.qas_datasphere_last_name}",
+      "email"      : "${var.qas_datasphere_email}",
+      "host_name"  : "${var.qas_datasphere_host_name}",
+    }
+  )
+  timeouts = {
+    create = "45m"
+    update = "45m"
+    delete = "45m"
+  }
+}
+
+# ------------------------------------------------------------------------------------------------------
+#  USERS AND ROLES
+# ------------------------------------------------------------------------------------------------------
+#
+# Get all available subaccount roles
+data "btp_subaccount_roles" "all" {
+  subaccount_id = btp_subaccount.dc_mission.id
+  depends_on    = [btp_subaccount_service_instance.datasphere]
+}
+
+# ------------------------------------------------------------------------------------------------------
+# Assign role collection "Subaccount Administrator"
+# ------------------------------------------------------------------------------------------------------
+resource "btp_subaccount_role_collection_assignment" "subaccount_admin" {
+  for_each             = toset("${var.subaccount_admins}")
+  subaccount_id        = btp_subaccount.dc_mission.id
+  role_collection_name = "Subaccount Administrator"
+  user_name            = each.value
+  depends_on           = [btp_subaccount.dc_mission]
+}
---
+++ released/discovery_center/mission_4108/sample.tfvars
@@ -0,0 +1,20 @@
+# ------------------------------------------------------------------------------------------------------
+# Provider configuration
+# ------------------------------------------------------------------------------------------------------
+# Your global account subdomain
+globalaccount = "xxxxxxxx-xxxxxxx-xxxxxxx-xxxxxxxx-xxxxxx"
+
+# The CLI server URL (needs to be set to null if you are using the default CLI server)
+cli_server_url = null
+
+# Region for your subaccount
+region = "us20"
+
+# Name of your sub account
+subaccount_name = "SAP Discovery Center Mission 4108"
+
+# ------------------------------------------------------------------------------------------------------
+# USER ROLES
+# ------------------------------------------------------------------------------------------------------
+subaccount_admins = ["another.user@test.com"]
+
