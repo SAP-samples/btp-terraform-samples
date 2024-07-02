e071c8b3b5fac899b8f408c7c882b7854db224b2
Rui Nogueira <rui.nogueira@sap.com>
2024-07-02 10:00:42 +0000

    remove modules

--- released/discovery_center/mission_4371/main.tf
+++ released/discovery_center/mission_4371/main.tf
@@ -17,24 +17,124 @@ resource "btp_subaccount" "gen_ai" {
 # ------------------------------------------------------------------------------------------------------
 # Prepare & setup the SAP AI Core service (ensure your global account has the respective entitlements)
 # ------------------------------------------------------------------------------------------------------
-module "ai_setup" {
-  source = "./modules/ai"
 
-  subaccount_id        = btp_subaccount.gen_ai.id
-  setup_ai_launchpad   = var.setup_ai_launchpad
-  ai_core_plan_name    = var.ai_core_plan_name
-  target_ai_core_model = var.target_ai_core_model
-  admins               = var.admins
-
+# Entitle subaccount for usage of SAP AI Core service
+# ------------------------------------------------------------------------------------------------------
+# Checkout https://github.com/SAP-samples/btp-service-metadata/blob/main/v0/developer/aicore.json for 
+# available plans and their region availability 
+resource "btp_subaccount_entitlement" "ai_core" {
+  subaccount_id = var.subaccount_id
+  service_name  = "aicore"
+  plan_name     = var.ai_core_plan_name
 }
 
-module "hana_cloud_setup" {
-  source = "./modules/hana-cloud"
-
-  subaccount_id        = btp_subaccount.gen_ai.id
-  hana_system_password = var.hana_system_password
-  admins               = var.admins
+# Get plan for SAP AI Core service
+data "btp_subaccount_service_plan" "ai_core" {
+  subaccount_id = var.subaccount_id
+  offering_name = "aicore"
+  name          = var.ai_core_plan_name
+  depends_on    = [btp_subaccount_entitlement.ai_core]
 }
+
+# Create service instance for SAP AI Core service
+resource "btp_subaccount_service_instance" "ai_core" {
+  subaccount_id  = var.subaccount_id
+  serviceplan_id = data.btp_subaccount_service_plan.ai_core.id
+  name           = "my-ai-core-instance"
+  depends_on     = [btp_subaccount_entitlement.ai_core]
+}
+
+# Create service binding to SAP AI Core service (exposed for a specific user group)
+resource "btp_subaccount_service_binding" "ai_core_binding" {
+  subaccount_id       = var.subaccount_id
+  service_instance_id = btp_subaccount_service_instance.ai_core.id
+  name                = "ai-core-key"
+}
+
+
+# ------------------------------------------------------------------------------------------------------
+# Entitle subaccount for usage of SAP HANA Cloud tools
+# ------------------------------------------------------------------------------------------------------
+resource "btp_subaccount_entitlement" "hana_cloud_tools" {
+  subaccount_id = var.subaccount_id
+  service_name  = "hana-cloud-tools"
+  plan_name     = "tools"
+}
+
+resource "btp_subaccount_subscription" "hana_cloud_tools" {
+  subaccount_id = var.subaccount_id
+  app_name      = "hana-cloud-tools"
+  plan_name     = "tools"
+  depends_on    = [btp_subaccount_entitlement.hana_cloud_tools]
+}
+
+# Assign users to Role Collection: SAP HANA Cloud Administrator
+resource "btp_subaccount_role_collection_assignment" "hana_cloud_admin" {
+  for_each             = toset(var.admins)
+  subaccount_id        = var.subaccount_id
+  role_collection_name = "SAP HANA Cloud Administrator"
+  user_name            = each.value
+  depends_on           = [btp_subaccount_subscription.hana_cloud_tools]
+}
+
+# ------------------------------------------------------------------------------------------------------
+# Entitle subaccount for usage of SAP HANA Cloud
+# ------------------------------------------------------------------------------------------------------
+resource "btp_subaccount_entitlement" "hana_cloud" {
+  subaccount_id = var.subaccount_id
+  service_name  = "hana-cloud"
+  plan_name     = "hana"
+}
+
+# Get plan for SAP HANA Cloud
+data "btp_subaccount_service_plan" "hana_cloud" {
+  subaccount_id = var.subaccount_id
+  offering_name = "hana-cloud"
+  name          = "hana"
+  depends_on    = [btp_subaccount_entitlement.hana_cloud]
+}
+
+resource "btp_subaccount_service_instance" "hana_cloud" {
+  subaccount_id  = var.subaccount_id
+  serviceplan_id = data.btp_subaccount_service_plan.hana_cloud.id
+  name           = "my-hana-cloud-instance"
+  depends_on     = [btp_subaccount_entitlement.hana_cloud]
+  parameters = jsonencode(
+    {
+      "data" : {
+        "memory" : 32,
+        "edition" : "cloud",
+        "systempassword" : "${var.hana_system_password}",
+        "additionalWorkers" : 0,
+        "disasterRecoveryMode" : "no_disaster_recovery",
+        "enabledservices" : {
+          "docstore" : false,
+          "dpserver" : true,
+          "scriptserver" : false
+        },
+        "requestedOperation" : {},
+        "serviceStopped" : false,
+        "slaLevel" : "standard",
+        "storage" : 120,
+        "vcpu" : 2,
+        "whitelistIPs" : ["0.0.0.0/0"]
+      }
+  })
+
+  timeouts = {
+    create = "45m"
+    update = "45m"
+    delete = "45m"
+  }
+}
+
+# Create service binding to SAP HANA Cloud service 
+resource "btp_subaccount_service_binding" "hana_cloud" {
+  subaccount_id       = var.subaccount_id
+  service_instance_id = btp_subaccount_service_instance.hana_cloud.id
+  name                = "hana-cloud-key"
+}
+
 
 resource "local_file" "env_file" {
   content  = join("\n", [module.ai_setup.ai_core_envs, module.hana_cloud_setup.hana_cloud_envs])
