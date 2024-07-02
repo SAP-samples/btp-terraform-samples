7e6e51d9fa6a6e2af689d139d518bcbff2d789b2
Rui Nogueira <rui.nogueira@sap.com>
2024-07-02 10:20:53 +0000

    udpate variables

--- released/discovery_center/mission_4371/main.tf
+++ released/discovery_center/mission_4371/main.tf
@@ -1,18 +1,50 @@
 # ------------------------------------------------------------------------------------------------------
 # Setup subaccount domain (to ensure uniqueness in BTP global account)
 # ------------------------------------------------------------------------------------------------------
-resource "random_id" "subaccount_domain_suffix" {
-  byte_length = 12
-}
+resource "random_uuid" "uuid" {}
 
+locals {
+  random_uuid       = random_uuid.uuid.result
+  subaccount_domain = lower(replace("mission-4371-${local.random_uuid}", "_", "-"))
+}
 # ------------------------------------------------------------------------------------------------------
 # Creation of subaccount
 # ------------------------------------------------------------------------------------------------------
-resource "btp_subaccount" "gen_ai" {
+resource "btp_subaccount" "dc_mission" {
   name      = var.subaccount_name
-  subdomain = join("-", ["genai-starter-kit", random_id.subaccount_domain_suffix.hex])
+  subdomain = local.subaccount_domain
   region    = lower(var.region)
 }
+
+# ------------------------------------------------------------------------------------------------------
+# Assign custom IDP to sub account (if custom_idp is set)
+# ------------------------------------------------------------------------------------------------------
+resource "btp_subaccount_trust_configuration" "fully_customized" {
+  # Only create trust configuration if custom_idp has been set 
+  count             = var.custom_idp == "" ? 0 : 1
+  subaccount_id     = btp_subaccount.dc_mission.id
+  identity_provider = var.custom_idp
+}
+
+# ------------------------------------------------------------------------------------------------------
+# Assignment of users as sub account administrators
+# ------------------------------------------------------------------------------------------------------
+resource "btp_subaccount_role_collection_assignment" "subaccount-admins" {
+  for_each             = toset(var.subaccount_admins)
+  subaccount_id        = btp_subaccount.dc_mission.id
+  role_collection_name = "Subaccount Administrator"
+  user_name            = each.value
+}
+# ------------------------------------------------------------------------------------------------------
+# Assignment of users as sub account service administrators
+# ------------------------------------------------------------------------------------------------------
+resource "btp_subaccount_role_collection_assignment" "subaccount-service-admins" {
+  for_each             = toset(var.subaccount_service_admins)
+  subaccount_id        = btp_subaccount.dc_mission.id
+  role_collection_name = "Subaccount Service Administrator"
+  user_name            = each.value
+}
+
 
 # ------------------------------------------------------------------------------------------------------
 # Prepare & setup the SAP AI Core service (ensure your global account has the respective entitlements)
@@ -23,14 +55,14 @@ resource "btp_subaccount" "gen_ai" {
 # Checkout https://github.com/SAP-samples/btp-service-metadata/blob/main/v0/developer/aicore.json for 
 # available plans and their region availability 
 resource "btp_subaccount_entitlement" "ai_core" {
-  subaccount_id = var.subaccount_id
+  subaccount_id = var.dc_mission.id
   service_name  = "aicore"
   plan_name     = var.ai_core_plan_name
 }
 
 # Get plan for SAP AI Core service
 data "btp_subaccount_service_plan" "ai_core" {
-  subaccount_id = var.subaccount_id
+  subaccount_id = btp_subaccount.dc_mission.id
   offering_name = "aicore"
   name          = var.ai_core_plan_name
   depends_on    = [btp_subaccount_entitlement.ai_core]
@@ -38,7 +70,7 @@ data "btp_subaccount_service_plan" "ai_core" {
 
 # Create service instance for SAP AI Core service
 resource "btp_subaccount_service_instance" "ai_core" {
-  subaccount_id  = var.subaccount_id
+  subaccount_id  = btp_subaccount.dc_mission.id
   serviceplan_id = data.btp_subaccount_service_plan.ai_core.id
   name           = "my-ai-core-instance"
   depends_on     = [btp_subaccount_entitlement.ai_core]
@@ -46,7 +78,7 @@ resource "btp_subaccount_service_instance" "ai_core" {
 
 # Create service binding to SAP AI Core service (exposed for a specific user group)
 resource "btp_subaccount_service_binding" "ai_core_binding" {
-  subaccount_id       = var.subaccount_id
+  subaccount_id       = btp_subaccount.dc_mission.id
   service_instance_id = btp_subaccount_service_instance.ai_core.id
   name                = "ai-core-key"
 }
@@ -56,13 +88,13 @@ resource "btp_subaccount_service_binding" "ai_core_binding" {
 # Entitle subaccount for usage of SAP HANA Cloud tools
 # ------------------------------------------------------------------------------------------------------
 resource "btp_subaccount_entitlement" "hana_cloud_tools" {
-  subaccount_id = var.subaccount_id
+  subaccount_id = btp_subaccount.dc_mission.id
   service_name  = "hana-cloud-tools"
   plan_name     = "tools"
 }
 
 resource "btp_subaccount_subscription" "hana_cloud_tools" {
-  subaccount_id = var.subaccount_id
+  subaccount_id = btp_subaccount.dc_mission.id
   app_name      = "hana-cloud-tools"
   plan_name     = "tools"
   depends_on    = [btp_subaccount_entitlement.hana_cloud_tools]
@@ -70,8 +102,8 @@ resource "btp_subaccount_subscription" "hana_cloud_tools" {
 
 # Assign users to Role Collection: SAP HANA Cloud Administrator
 resource "btp_subaccount_role_collection_assignment" "hana_cloud_admin" {
-  for_each             = toset(var.admins)
-  subaccount_id        = var.subaccount_id
+  for_each             = toset(var.hana_cloud_admins)
+  subaccount_id        = btp_subaccount.dc_mission.id
   role_collection_name = "SAP HANA Cloud Administrator"
   user_name            = each.value
   depends_on           = [btp_subaccount_subscription.hana_cloud_tools]
@@ -81,21 +113,21 @@ resource "btp_subaccount_role_collection_assignment" "hana_cloud_admin" {
 # Entitle subaccount for usage of SAP HANA Cloud
 # ------------------------------------------------------------------------------------------------------
 resource "btp_subaccount_entitlement" "hana_cloud" {
-  subaccount_id = var.subaccount_id
+  subaccount_id = btp_subaccount.dc_mission.id
   service_name  = "hana-cloud"
   plan_name     = "hana"
 }
 
 # Get plan for SAP HANA Cloud
 data "btp_subaccount_service_plan" "hana_cloud" {
-  subaccount_id = var.subaccount_id
+  subaccount_id = btp_subaccount.dc_mission.id
   offering_name = "hana-cloud"
   name          = "hana"
   depends_on    = [btp_subaccount_entitlement.hana_cloud]
 }
 
 resource "btp_subaccount_service_instance" "hana_cloud" {
-  subaccount_id  = var.subaccount_id
+  subaccount_id  = btp_subaccount.dc_mission.id
   serviceplan_id = data.btp_subaccount_service_plan.hana_cloud.id
   name           = "my-hana-cloud-instance"
   depends_on     = [btp_subaccount_entitlement.hana_cloud]
@@ -130,7 +162,7 @@ resource "btp_subaccount_service_instance" "hana_cloud" {
 
 # Create service binding to SAP HANA Cloud service 
 resource "btp_subaccount_service_binding" "hana_cloud" {
-  subaccount_id       = var.subaccount_id
+  subaccount_id       = btp_subaccount.dc_mission.id
   service_instance_id = btp_subaccount_service_instance.hana_cloud.id
   name                = "hana-cloud-key"
 }
--- released/discovery_center/mission_4371/variables.tf
+++ released/discovery_center/mission_4371/variables.tf
@@ -1,24 +1,91 @@
+######################################################################
+# Customer account setup
+######################################################################
 variable "globalaccount" {
   type        = string
-  description = "The globalaccount subdomain where the sub account shall be created."
-}
-
-variable "setup_ai_launchpad" {
-  type        = bool
-  description = "Switch to enable the setup of the AI Launchpad."
-  default     = true
-}
-
-variable "subaccount_name" {
-  type        = string
-  description = "The subaccount name."
-  default     = "My SAP Build Apps subaccount."
+  description = "Defines the global account"
+  default     = "yourglobalaccount"
 }
 
 variable "cli_server_url" {
   type        = string
-  description = "The BTP CLI server URL."
-  default     = "https://cpcli.cf.sap.hana.ondemand.com"
+  description = "Defines the CLI server URL"
+  default     = "https://cli.btp.cloud.sap"
+}
+
+# subaccount
+variable "subaccount_name" {
+  type        = string
+  description = "The subaccount name."
+  default     = "SAP Discovery Center Mission 3774 - Central Inbox with SAP Task Center"
+}
+variable "subaccount_id" {
+  type        = string
+  description = "The subaccount ID."
+  default     = ""
+}
+# Region
+variable "region" {
+  type        = string
+  description = "The region where the project account shall be created in."
+  default     = "us10"
+}
+
+variable "subaccount_admins" {
+  type        = list(string)
+  description = "Defines the colleagues who are added to each subaccount as subaccount administrators."
+  default     = ["jane.doe@test.com", "john.doe@test.com"]
+
+  # add validation to check if admins contains a list of valid email addresses
+  validation {
+    condition     = length([for email in var.subaccount_admins : can(regex("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$", email))]) == length(var.subaccount_admins)
+    error_message = "Please enter a valid email address for the CF space managers."
+  }
+}
+
+variable "subaccount_service_admins" {
+  type        = list(string)
+  description = "Defines the colleagues who are added to each subaccount as subaccount service administrators."
+  default     = ["jane.doe@test.com", "john.doe@test.com"]
+
+  # add validation to check if admins contains a list of valid email addresses
+  validation {
+    condition     = length([for email in var.subaccount_service_admins : can(regex("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$", email))]) == length(var.subaccount_service_admins)
+    error_message = "Please enter a valid email address for the CF space managers."
+  }
+}
+
+variable "hana_cloud_admins" {
+  type        = list(string)
+  description = "Defines the colleagues who are added as admins to access the instance of SAP HANA Cloud."
+  default     = ["jane.doe@test.com", "john.doe@test.com"]
+
+  # add validation to check if admins contains a list of valid email addresses
+  validation {
+    condition     = length([for email in var.hana_cloud_admins : can(regex("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$", email))]) == length(var.hana_cloud_admins)
+    error_message = "Please enter a valid email address for the admins of SAP HANA Cloud instance."
+  }
+}
+
+variable "custom_idp" {
+  type        = string
+  description = "Defines the custom IdP"
+  default     = ""
+}
+
+variable "origin_key" {
+  type        = string
+  description = "Defines the origin key of the identity provider"
+  default     = "sap.ids"
+  # The value for the origin_key can be defined
+  # but are normally set to "sap.ids", "sap.default" or "sap.custom"
+}
+
+
+variable "create_tfvars_file_for_step2" {
+  type        = bool
+  description = "Switch to enable the creation of the tfvars file for step 2."
+  default     = false
 }
 
 variable "ai_core_plan_name" {
@@ -48,9 +115,9 @@ variable "hana_system_password" {
     error_message = "The hana_system_password must contain at least one upper case."
   }
 
-  # add validation to check if the password contains at least two lower case characters
+  # add validation to check if the password contains at least two lower case characters that can occur on arbitrary places in the string (not necessarily in a row)
   validation {
-    condition     = can(regex("[a-z]{2}", var.hana_system_password))
+    condition     = length(regexall("[a-z]", var.hana_system_password)) > 1
     error_message = "The hana_system_password must contain at least two lower case characters."
   }
 
@@ -69,7 +136,7 @@ variable "target_ai_core_model" {
   validation {
     condition = length([
       for o in var.target_ai_core_model : true
-      if contains(["gpt-35-turbo", "gpt-35-turbo-16k", "gpt-4", "gpt-4-32k", "text-embedding-ada-002", "tiiuae--falcon-40b-instruct"], o)
+      if contains(["gpt-35-turbo", "gpt-35-turbo-0125", "gpt-35-turbo-16k", "gpt-4", "gpt-4-32k", "text-embedding-ada-002", "gemini-1.0-pro", "text-bison", "chat-bison", "textembedding-gecko-multilingual", "textembedding-gecko", "tiiuae--falcon-40b-instruct"], o)
     ]) == length(var.target_ai_core_model)
     error_message = "Please enter a valid entry for the target_ai_core_model of the AI Core service. Valid values are: gpt-35-turbo, gpt-35-turbo-16k, gpt-4, gpt-4-32k, text-embedding-ada-002, tiiuae--falcon-40b-instruct."
   }
@@ -85,16 +152,5 @@ variable "region" {
   validation {
     condition     = contains(["eu10-canary", "ap10", "eu10", "eu11", "jp10", "us10"], var.region)
     error_message = "Please enter a valid region for the sub account. Checkout https://github.com/SAP-samples/btp-service-metadata/blob/main/v0/developer/aicore.json for regions providing the AI Core service."
-  }
-}
-
-variable "admins" {
-  type        = list(string)
-  description = "Defines the colleagues who are added to each subaccount as emergency administrators."
-
-  # add validation to check if admins contains a list of valid email addresses
-  validation {
-    condition     = length([for email in var.admins : can(regex("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$", email))]) == length(var.admins)
-    error_message = "Please enter a valid email address for the admins."
   }
 }
