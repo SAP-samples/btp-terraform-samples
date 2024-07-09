c1a9d1e157cd1a029e95ffa20275652e5f0df495
Rui Nogueira <rui.nogueira@sap.com>
2024-07-09 07:11:02 +0000

    updates variables

---
+++ tests/compliance/libs/constants/outputs.py
@@ -0,0 +1,6 @@
+# ------------------------------------------------------------------
+# Terraform outputs
+# ------------------------------------------------------------------
+# BTP Provider
+QAS_STEP1_BTP_PROVIDER_MANDATORY_OUTPUTS = [
+    "cf_org_admins", "cf_org_name", "cf_space_developers", "cf_space_managers", "cf_space_name", "cf_landscape_label", "origin"]
