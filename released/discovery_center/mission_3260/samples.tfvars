8201a77f0a3f13f4214c36c1839e4026039a42be
Rui Nogueira <rui1610@users.noreply.github.com>
2024-07-10 14:31:31 +0200

    clean-up code for usage in quick-account-setup (mission 3260) (#245)
    * clean-up code for usage in quick-account-setup
    
    * format files and change outputs.tf file
    
    * update provider setup and add missing variables
    
    * address review results from markus
    
    * updat format

--- released/discovery_center/mission_3260/samples.tfvars
+++ released/discovery_center/mission_3260/samples.tfvars
@@ -2,13 +2,15 @@
 # Provider configuration
 # ------------------------------------------------------------------------------------------------------
 # Your global account subdomain
-globalaccount        = "youraccount"
-region               = "us10"
-subaccount_name      = "Discovery Center mission - build Events-to-Business actions"
-cf_environment_label = "cf-us10"
+globalaccount   = "youraccount"
+region          = "us10"
+subaccount_name = "Discovery Center mission 3260 - build Events-to-Business actions"
+#cf_landscape_label = "cf-us10"
+#custom_idp         = "xxxx"
 
 # ------------------------------------------------------------------------------------------------------
 # Project specific configuration (please adapt!)
 # ------------------------------------------------------------------------------------------------------
-subaccount_admins         = ["jane.doe@test.com", "john.doe@test.com"]
-subaccount_service_admins = ["jane.doe@test.com", "john.doe@test.com"]
+subaccount_admins                  = ["another.user@test.com"]
+subaccount_service_admins          = ["another.user@test.com"]
+business_process_automation_admins = ["another.user@test.com", "you@test.com"]
--- released/discovery_center/mission_3260/variables.tf
+++ released/discovery_center/mission_3260/variables.tf
@@ -13,17 +13,43 @@ variable "subaccount_name" {
   description = "The subaccount name."
   default     = "UC - Build resilient BTP Apps"
 }
+
+variable "subaccount_id" {
+  type        = string
+  description = "The subaccount ID."
+  default     = ""
+}
+
+variable "cli_server_url" {
+  type        = string
+  description = "Defines the CLI server URL"
+  default     = "https://cli.btp.cloud.sap"
+}
+
+variable "custom_idp" {
+  type        = string
+  description = "Defines the custom IdP"
+  default     = ""
+}
+
 # Region
 variable "region" {
   type        = string
   description = "The region where the project account shall be created in."
   default     = "us10"
 }
-# Cloudfoundry environment label
-variable "cf_environment_label" {
+
+variable "cf_org_name" {
   type        = string
-  description = "The Cloudfoundry environment label"
-  default     = "cf-us10"
+  description = "The Cloud Foundry Org name from the Cloud Foundry environment instance."
+  default     = ""
+}
+
+# Cloudfoundry environment label
+variable "cf_landscape_label" {
+  type        = string
+  description = "In case there are multiple environments available for a subaccount, you can use this label to choose with which one you want to go. If nothing is given, we take by default the first available."
+  default     = ""
 }
 
 variable "subaccount_admins" {
@@ -35,5 +61,11 @@ variable "subaccount_admins" {
 variable "subaccount_service_admins" {
   type        = list(string)
   description = "Defines the colleagues who are added to each subaccount as subaccount service administrators."
+  default     = ["jane.doe@test.com", "john.doe@test.com"]
+}
+
+variable "business_process_automation_admins" {
+  type        = list(string)
+  description = "Defines the colleagues who are added as administrators for the SAP Business Process Automation."
   default     = ["jane.doe@test.com", "john.doe@test.com"]
 }
