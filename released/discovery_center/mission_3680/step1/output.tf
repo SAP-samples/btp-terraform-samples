0b6b08b5e7c8b79c4e5854f57d910f50f2d9a95b
Mahesh Palavalli <p.maheshkumarreddy@gmail.com>
2024-07-11 17:42:24 +0530

    [NEW] dc mission 3680 (#254)
    * [NEW] mission 3680
    * remove step2 switch in samples.tfvars
    
    ---------
    
    Co-authored-by: Rui Nogueira <rui1610@users.noreply.github.com>

---
+++ released/discovery_center/mission_3680/step1/output.tf
@@ -0,0 +1,29 @@
+output "subaccount_id" {
+  value       = btp_subaccount.dc_mission.id
+  description = "The ID of the subaccount."
+}
+
+output "cf_api_url" {
+  value       = jsondecode(btp_subaccount_environment_instance.cloudfoundry.labels)["API Endpoint"]
+  description = "The Cloudfoundry API endpoint."
+}
+
+output "cf_landscape_label" {
+  value       = terraform_data.replacement.output
+  description = "The Cloudfoundry landscape label."
+}
+
+output "cf_org_id" {
+  value       = jsondecode(btp_subaccount_environment_instance.cloudfoundry.labels)["Org ID"]
+  description = "The Cloudfoundry org id."
+}
+
+output "event_mesh_url" {
+  value       = btp_subaccount_subscription.event_mesh_application.subscription_url
+  description = "Event Mesh URL"
+}
+
+# output "hana_tools_url" {
+#   value       = btp_subaccount_subscription.hana_cloud_tools.subscription_url
+#   description = "HANA Tools URL"
+# }
