f3a50cf997b437881e3acb054df35c6241ec2b07
Markus Balsam <m.balsam@sap.com>
2024-07-23 11:54:00 +0200

    Add output to mission 3248

--- released/discovery_center/mission_3248_trial/step2/outputs.tf
+++ released/discovery_center/mission_3248_trial/step2/outputs.tf
@@ -3,6 +3,11 @@ output "abab_trial_service_instance_id" {
   description = "The ID of the ABAP service instance."
 }
 
+output "abap_trial_dashboard_url" {
+  value = cloudfoundry_service_instance.abap_trial.dashboard_url
+  description = "The URL of the ABAP Trial dashboard."
+}
+
 output "abab_trial_service_key_id" {
   value       = cloudfoundry_service_credential_binding.abap_trial_service_key.id
   description = "The ID of the ABAP service key."
