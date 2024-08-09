2ea83946bfc9cdcdcced4a44f6e8bd88271b64aa
prajin-op <prajin.op@gmail.com>
2024-08-09 09:56:06 +0530

    chore: Update provider to version 1.5.0

--- released/discovery_center/mission_4327/step2_kyma/provider.tf
+++ released/discovery_center/mission_4327/step2_kyma/provider.tf
@@ -2,11 +2,7 @@ terraform {
   required_providers {
     btp = {
       source  = "sap/btp"
-      version = "~> 1.4.0"
-    }
-    cloudfoundry = {
-      source  = "cloudfoundry-community/cloudfoundry"
-      version = "~> 0.53.1"
+      version = "~> 1.5.0"
     }
   }
 }
@@ -16,11 +12,6 @@ terraform {
 provider "btp" {
   globalaccount  = var.globalaccount
   cli_server_url = var.cli_server_url
-}
-
-// Configuration is described in https://registry.terraform.io/providers/cloudfoundry-community/cloudfoundry/latest/docs
-provider "cloudfoundry" {
-  api_url = "https://api.cf.${var.region}.hana.ondemand.com"
 }
 
 
