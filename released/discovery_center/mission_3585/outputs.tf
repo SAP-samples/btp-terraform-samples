e1ba542e9c2e9e74b0d188f92b9352a8fee722b9
Jens Glander <jens.glander@sap.com>
2024-09-04 17:20:33 +0200

    3585: removed not used cicd service

--- released/discovery_center/mission_3585/outputs.tf
+++ released/discovery_center/mission_3585/outputs.tf
@@ -13,10 +13,6 @@ output "sap_launchpad_subscription_url" {
   description = "SAP Build Work Zone, standard edition subscription URL."
 }
 
-output "cicd_service_id" {
-  value = var.use_optional_resources ? btp_subaccount_service_instance.cicd_service[0].id : null
-}
-
 output "cicd_app_subscription_url" {
   value       = var.use_optional_resources ? btp_subaccount_subscription.cicd_app[0].subscription_url : null
   description = "Continuous Integration & Delivery subscription URL."
