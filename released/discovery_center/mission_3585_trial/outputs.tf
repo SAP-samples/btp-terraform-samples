7eec32c0f6a39ae4decbd771b787a52b90406d40
jglander <jens.glander@sap.com>
2024-09-05 10:01:38 +0200

    3585_trial: add cicd (optional app subscription) (#304)
    * 3585_trial: add cicd (optional app subscription)
    
    * fixed tf formatting issues

--- released/discovery_center/mission_3585_trial/outputs.tf
+++ released/discovery_center/mission_3585_trial/outputs.tf
@@ -7,3 +7,8 @@ output "sap_launchpad_subscription_url" {
   value       = data.btp_subaccount_subscription.sap_launchpad.subscription_url
   description = "SAP Build Work Zone, standard edition subscription URL."
 }
+
+output "cicd_app_subscription_url" {
+  value       = var.use_optional_resources ? btp_subaccount_subscription.cicd_app[0].subscription_url : null
+  description = "Continuous Integration & Delivery subscription URL."
+}
