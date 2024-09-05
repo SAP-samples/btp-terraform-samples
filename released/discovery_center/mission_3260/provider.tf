021613907528b46e7a8f22a437349e1d40861d99
jglander <jens.glander@sap.com>
2024-09-05 09:56:41 +0200

    3260: Polishing the QAS enabled Mission (#306)
    - polished code to be consistent with other QAS missions (like 4024 or 3585)

--- released/discovery_center/mission_3260/provider.tf
+++ released/discovery_center/mission_3260/provider.tf
@@ -1,16 +1,13 @@
-
 terraform {
   required_providers {
     btp = {
-      source  = "sap/btp"
+      source  = "SAP/btp"
       version = "~> 1.5.0"
     }
   }
 }
 
-# Please checkout documentation on how best to authenticate against SAP BTP
-# via the Terraform provider for SAP BTP
 provider "btp" {
-  globalaccount  = var.globalaccount
   cli_server_url = var.cli_server_url
+  globalaccount  = var.globalaccount
 }
--- released/discovery_center/mission_3260/variables.tf
+++ released/discovery_center/mission_3260/variables.tf
@@ -1,63 +1,48 @@
-######################################################################
-# Customer account setup
-######################################################################
-# subaccount
+# ------------------------------------------------------------------------------------------------------
+# Account variables
+# ------------------------------------------------------------------------------------------------------
 variable "globalaccount" {
   type        = string
-  description = "The globalaccount subdomain."
-  default     = "yourglobalaccount"
+  description = "The globalaccount subdomain where the sub account shall be created."
 }
-# subaccount
+
+variable "cli_server_url" {
+  type        = string
+  description = "The BTP CLI server URL."
+  default     = "https://cli.btp.cloud.sap"
+}
+
+variable "custom_idp" {
+  type        = string
+  description = "The custom identity provider for the subaccount."
+  default     = ""
+}
+
+variable "region" {
+  type        = string
+  description = "The region where the subaccount shall be created in."
+  default     = "us10"
+}
+
 variable "subaccount_name" {
   type        = string
   description = "The subaccount name."
-  default     = "DC Mission 3260 - Process and approve your invoices with SAP Build Process Automation"
+  default     = "My SAP DC mission subaccount."
 }
 
-# subaccount id
 variable "subaccount_id" {
   type        = string
   description = "The subaccount ID."
   default     = ""
 }
 
-variable "cli_server_url" {
-  type        = string
-  description = "Defines the CLI server URL"
-  default     = "https://cli.btp.cloud.sap"
-}
-
-variable "custom_idp" {
-  type        = string
-  description = "Defines the custom IdP"
-  default     = ""
-}
-
-# Region
-variable "region" {
-  type        = string
-  description = "The region where the project account shall be created in."
-  default     = "us10"
-}
-
-variable "subaccount_admins" {
-  type        = list(string)
-  description = "Defines the colleagues who are added to each subaccount as subaccount administrators."
-  default     = ["jane.doe@test.com", "john.doe@test.com"]
-}
-
-variable "subaccount_service_admins" {
-  type        = list(string)
-  description = "Defines the colleagues who are added to each subaccount as subaccount service administrators."
-  default     = ["jane.doe@test.com", "john.doe@test.com"]
-}
-
-
-# Process automation Variables
+# ------------------------------------------------------------------------------------------------------
+# app subscription plans
+# ------------------------------------------------------------------------------------------------------
 variable "service_plan__sap_process_automation" {
   type        = string
-  description = "The plan for SAP Build Process Automation"
-  default     = "standard"
+  description = "The plan for service 'SAP Build Process Automation' with technical name 'process-automation'"
+  default     = "free"
 
   validation {
     condition     = contains(["standard", "free"], var.service_plan__sap_process_automation)
@@ -65,9 +50,22 @@ variable "service_plan__sap_process_automation" {
   }
 }
 
+# ------------------------------------------------------------------------------------------------------
+# User lists
+# ------------------------------------------------------------------------------------------------------
+variable "subaccount_admins" {
+  type        = list(string)
+  description = "Defines the users who are added to subaccount as administrators."
+}
+
+variable "subaccount_service_admins" {
+  type        = list(string)
+  description = "Defines the users who are added to subaccount as service administrators."
+}
+
 variable "process_automation_admins" {
   type        = list(string)
-  description = "Defines the users who have the role of ProcessAutomationAdmin in SAP Build Process Automation"
+  description = "Defines the users who have the role of 'ProcessAutomationAdmin' in SAP Build Process Automation."
 }
 
 variable "process_automation_developers" {
