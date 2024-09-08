85e2e8d42a8e2d1f99de257309aa338403a98ba0
navyakhurana <navya.khurana@sap.com>
2024-09-08 21:06:09 +0530

    Added free plan for integration suite & minor fix

--- released/discovery_center/mission_4356/step1/provider.tf
+++ released/discovery_center/mission_4356/step1/provider.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     btp = {
-      source  = "sap/btp"
+      source  = "SAP/btp"
       version = "1.5.0"
     }
   }
--- released/discovery_center/mission_4356/step2/provider.tf
+++ released/discovery_center/mission_4356/step2/provider.tf
@@ -1,10 +1,22 @@
 terraform {
   required_providers {
+    btp = {
+      source  = "SAP/btp"
+      version = "1.5.0"
+    }
     cloudfoundry = {
       source  = "SAP/cloudfoundry"
       version = "1.0.0-rc1"
     }
   }
+}
+
+######################################################################
+# Configure BTP provider
+######################################################################
+provider "btp" {
+  cli_server_url = var.cli_server_url
+  globalaccount  = var.globalaccount
 }
 
 ######################################################################
