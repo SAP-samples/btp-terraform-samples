352e6abd17206199435a925a34a8e02ea5eb5bac
Jens Glander <jens.glander@sap.com>
2024-09-17 09:46:54 +0200

    added missing output changes

--- released/discovery_center/mission_3260/step1/outputs.tf
+++ released/discovery_center/mission_3260/step1/outputs.tf
@@ -1,3 +1,13 @@
+output "globalaccount" {
+  value       = var.globalaccount
+  description = "The globalaccount subdomain."
+}
+
+output "cli_server_url" {
+  value       = var.cli_server_url
+  description = "The BTP CLI server URL."
+}
+
 output "subaccount_id" {
   value       = data.btp_subaccount.dc_mission.id
   description = "The ID of the dc mission subaccount."
@@ -6,4 +16,49 @@ output "subaccount_id" {
 output "process_automation_subscription_url" {
   value       = btp_subaccount_subscription.build_process_automation.subscription_url
   description = "Subscription URL for SAP Build Process Automation"
+}
+
+output "cf_api_url" {
+  value       = jsondecode(btp_subaccount_environment_instance.cloudfoundry.labels)["API Endpoint"]
+  description = "The Cloudfoundry API endpoint."
+}
+
+output "cf_landscape_label" {
+  value       = btp_subaccount_environment_instance.cloudfoundry.landscape_label
+  description = "The Cloudfoundry landscape label."
+}
+
+output "cf_org_id" {
+  value       = jsondecode(btp_subaccount_environment_instance.cloudfoundry.labels)["Org ID"]
+  description = "The Cloudfoundry org id."
+}
+
+output "cf_org_name" {
+  value       = jsondecode(btp_subaccount_environment_instance.cloudfoundry.labels)["Org Name"]
+  description = "The Cloudfoundry org name."
+}
+
+output "cf_space_name" {
+  value       = var.cf_space_name
+  description = "The name of the Cloud Foundry space."
+}
+
+output "cf_org_admins" {
+  value       = var.cf_org_admins
+  description = "List of users to set as Cloudfoundry org administrators."
+}
+
+output "cf_space_developers" {
+  value       = var.cf_space_developers
+  description = "List of users to set as Cloudfoundry space developers."
+}
+
+output "cf_space_managers" {
+  value       = var.cf_space_managers
+  description = "List of users to set as Cloudfoundry space managers."
+}
+
+output "origin" {
+  value       = var.origin
+  description = "The identity provider for the UAA user."
 }
