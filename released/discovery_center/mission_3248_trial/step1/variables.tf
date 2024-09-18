e831a48e8ee16b889f211449f5a07add2691770b
Jens Glander <jens.glander@sap.com>
2024-09-18 14:31:24 +0200

    3248_trial: Polished the QAS enabled Mission

--- released/discovery_center/mission_3248_trial/step1/variables.tf
+++ released/discovery_center/mission_3248_trial/step1/variables.tf
@@ -1,12 +1,9 @@
+# ------------------------------------------------------------------------------------------------------
+# Account variables
+# ------------------------------------------------------------------------------------------------------
 variable "globalaccount" {
   type        = string
-  description = "The subdomain of the trial account."
-}
-
-variable "subaccount_id" {
-  type        = string
-  description = "The ID of the trial subaccount."
-  default     = ""
+  description = "The globalaccount subdomain where the sub account shall be created."
 }
 
 variable "cli_server_url" {
@@ -15,6 +12,70 @@ variable "cli_server_url" {
   default     = "https://cli.btp.cloud.sap"
 }
 
+variable "region" {
+  type        = string
+  description = "The region where the subaccount shall be created in."
+  default     = "us10"
+}
+
+variable "subaccount_name" {
+  type        = string
+  description = "The subaccount name."
+  default     = "My SAP DC mission trial subaccount."
+}
+
+variable "subaccount_id" {
+  type        = string
+  description = "The ID of the trial subaccount."
+  default     = ""
+}
+# ------------------------------------------------------------------------------------------------------
+# use case specific variables
+# ------------------------------------------------------------------------------------------------------
+variable "abap_admin_email" {
+  type        = string
+  description = "Email of the ABAP Administrator."
+  default     = ""
+}
+
+variable "create_cf_space" {
+  type        = bool
+  description = "Determines whether a new CF space should be created. Must be true if no space with the given name exists for the org, false otherwise. If CF isn't enabled for no subaccount a new space will always be created"
+  default     = false
+}
+
+variable "cf_space_name" {
+  type        = string
+  description = "The name of the CF space to use."
+  default     = "dev"
+}
+
+# ------------------------------------------------------------------------------------------------------
+# service plans
+# ------------------------------------------------------------------------------------------------------
+variable "service_plan__abap_trial" {
+  type        = string
+  description = "The plan for service 'ABAP environment' with technical name 'abap-trial'"
+  default     = "shared"
+  validation {
+    condition     = contains(["shared"], var.service_plan__abap_trial)
+    error_message = "Invalid value for service_plan__abap_trial. Only 'shared' is allowed."
+  }
+}
+
+variable "service_plan__cloudfoundry" {
+  type        = string
+  description = "The plan for service 'Destination Service' with technical name 'destination'"
+  default     = "trial"
+  validation {
+    condition     = contains(["trial"], var.service_plan__cloudfoundry)
+    error_message = "Invalid value for service_plan__cloudfoundry. Only 'trial' is allowed."
+  }
+}
+
+# ------------------------------------------------------------------------------------------------------
+# User lists
+# ------------------------------------------------------------------------------------------------------
 variable "cf_org_managers" {
   type        = list(string)
   description = "List of managers for the Cloud Foundry org."
@@ -24,32 +85,20 @@ variable "cf_org_managers" {
 variable "cf_space_managers" {
   type        = list(string)
   description = "List of managers for the Cloud Foundry space."
+  default     = []
 }
 
 variable "cf_space_developers" {
   type        = list(string)
   description = "List of developers for the Cloud Foundry space."
+  default     = []
 }
 
-variable "cf_space_name" {
-  type        = string
-  description = "The name of the CF space to use."
-}
-
-variable "create_cf_space" {
+# ------------------------------------------------------------------------------------------------------
+# Switch for creating tfvars for step 2
+# ------------------------------------------------------------------------------------------------------
+variable "create_tfvars_file_for_step2" {
   type        = bool
-  description = "Determines whether a new CF space should be created. Must be true if no space with the given name exists for the org, false otherwise. If CF isn't enabled for the subaccount a new space will always be created"
-  default     = false
-}
-
-variable "abap_admin_email" {
-  type        = string
-  description = "Email of the ABAP Administrator."
-  default     = ""
-}
-
-variable "create_tfvars_file_for_next_stage" {
-  type        = bool
-  description = "Switch to enable the creation of the tfvars file for the next step."
+  description = "Switch to enable the creation of the tfvars file for step 2."
   default     = false
 }
--- released/discovery_center/mission_3248_trial/step2/provider.tf
+++ released/discovery_center/mission_3248_trial/step2/provider.tf
@@ -1,13 +1,22 @@
 terraform {
   required_providers {
     cloudfoundry = {
-      source  = "sap/cloudfoundry"
+      source  = "SAP/cloudfoundry"
       version = "1.0.0-rc1"
+    }
+    btp = {
+      source  = "SAP/btp"
+      version = "~> 1.5.0"
     }
   }
 }
 
-# This will only work if we know the region in advance
+provider "btp" {
+  globalaccount  = var.globalaccount
+  cli_server_url = var.cli_server_url
+}
+
+
 provider "cloudfoundry" {
   api_url = var.cf_api_url
 }
