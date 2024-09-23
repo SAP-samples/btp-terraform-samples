f8ebee08caa3d9e530ecd6c66b632861d189f4c3
mahesh0431 <p.maheshkumarreddy@gmail.com>
2024-09-23 09:37:19 +0530

    fix dashboard URL

--- released/discovery_center/mission_3348/outputs.tf
+++ released/discovery_center/mission_3348/outputs.tf
@@ -2,3 +2,7 @@ output "subaccount_id" {
   value       = data.btp_subaccount.dc_mission.id
   description = "The ID of the subaccount."
 }
+
+output "sac_instance_dashboard_url" {
+  value = btp_subaccount_service_instance.sac.dashboard_url
+}
