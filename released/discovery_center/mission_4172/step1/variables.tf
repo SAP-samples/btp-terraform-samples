ec34bc65f4ee502ccee11fa52f90bb0be97857a0
Mahesh Palavalli <p.maheshkumarreddy@gmail.com>
2024-09-24 12:22:41 +0530

    DC Mission 4172 - Polished for QAS  (#322)

--- released/discovery_center/mission_4172/step1/variables.tf
+++ released/discovery_center/mission_4172/step1/variables.tf
@@ -49,27 +49,42 @@ variable "cli_server_url" {
   default     = "https://cli.btp.cloud.sap"
 }
 
+variable "custom_idp" {
+  type        = string
+  description = "The custom identity provider for the subaccount."
+  default     = ""
+}
+
+variable "custom_idp_apps_origin_key" {
+  type        = string
+  description = "The custom identity provider for the subaccount."
+  default     = "sap.custom"
+}
+
+variable "origin" {
+  type        = string
+  description = "Defines the origin key of the identity provider"
+  default     = "sap.ids"
+  # The value for the origin_key can be defined
+  # but are normally set to "sap.ids", "sap.default" or "sap.custom"
+}
+
+variable "origin_key" {
+  type        = string
+  description = "Defines the origin key of the identity provider"
+  default     = ""
+  # The value for the origin_key can be defined, set to "sap.ids", "sap.default" or "sap.custom"
+}
+
 # subaccount variables
 variable "subaccount_admins" {
   type        = list(string)
   description = "Defines the colleagues who are added to each subaccount as subaccount administrators."
-  default     = ["jane.doe@test.com", "john.doe@test.com"]
-  # add validation to check if admins contains a list of valid email addresses
-  validation {
-    condition     = length([for email in var.subaccount_admins : can(regex("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$", email))]) == length(var.subaccount_admins)
-    error_message = "Please enter a valid email address for the Subaccount Admins."
-  }
 }
 
 variable "subaccount_service_admins" {
   type        = list(string)
   description = "Defines the colleagues who are added to each subaccount as subaccount service administrators."
-  default     = ["jane.doe@test.com", "john.doe@test.com"]
-  # add validation to check if admins contains a list of valid email addresses
-  validation {
-    condition     = length([for email in var.subaccount_service_admins : can(regex("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$", email))]) == length(var.subaccount_service_admins)
-    error_message = "Please enter a valid email address for the Subaccount service Admins."
-  }
 }
 
 variable "service_plan__sap_business_app_studio" {
@@ -82,94 +97,19 @@ variable "service_plan__sap_business_app_studio" {
   }
 }
 
-###
-# Entitlements
-###
-variable "entitlements" {
-  type = list(object({
-    service_name = string
-    plan_name    = string
-    type         = string
-  }))
-  description = "The list of entitlements that shall be added to the subaccount."
-  default = [
-    {
-      service_name = "connectivity"
-      plan_name    = "lite",
-      type         = "service"
-    },
-    {
-      service_name = "destination"
-      plan_name    = "lite",
-      type         = "service"
-    },
-    {
-      service_name = "html5-apps-repo"
-      plan_name    = "app-host",
-      type         = "service"
-    },
-    {
-      service_name = "xsuaa"
-      plan_name    = "application",
-      type         = "service"
-    }
-  ]
-}
-
 variable "appstudio_developers" {
   type        = list(string)
   description = "Business Application Studio Developers"
-  default     = ["jane.doe@test.com", "john.doe@test.com"]
-  # add validation to check if Business Application Studio Developers contains a list of valid email addresses
-  validation {
-    condition     = length([for email in var.appstudio_developers : can(regex("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$", email))]) == length(var.appstudio_developers)
-    error_message = "Please enter a valid email address for the Business Application Studio Developers"
-  }
 }
 
 variable "appstudio_admins" {
   type        = list(string)
   description = "Business Application Studio Administrators"
-  default     = ["jane.doe@test.com", "john.doe@test.com"]
-  # add validation to check if Business Application Studio Administrators contains a list of valid email addresses
-  validation {
-    condition     = length([for email in var.appstudio_admins : can(regex("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$", email))]) == length(var.appstudio_admins)
-    error_message = "Please enter a valid email address for the Business Application Studio Administrators."
-  }
 }
 
-variable "cloudconnector_admins" {
-  type        = list(string)
-  description = "Cloud Connector Administrators"
-  default     = ["jane.doe@test.com", "john.doe@test.com"]
-  # add validation to check if Cloud Connector Administrators contains a list of valid email addresses
-  validation {
-    condition     = length([for email in var.cloudconnector_admins : can(regex("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$", email))]) == length(var.cloudconnector_admins)
-    error_message = "Please enter a valid email address for the Cloud Connector Administrators."
-  }
-}
-
-variable "conn_dest_admins" {
-  type        = list(string)
-  description = "Connectivity and Destination Administrators"
-  default     = ["jane.doe@test.com", "john.doe@test.com"]
-  # add validation to check if Connectivity and Destination Administrators contains a list of valid email addresses
-  validation {
-    condition     = length([for email in var.conn_dest_admins : can(regex("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$", email))]) == length(var.conn_dest_admins)
-    error_message = "Please enter a valid email address for the Connectivity and Destination Administrators."
-  }
-}
-
-variable "hana_cloud_admins" {
-  type        = list(string)
-  description = "Defines the colleagues who are added as admins to access the instance of SAP HANA Cloud."
-  default     = ["jane.doe@test.com", "john.doe@test.com"]
-
-  # add validation to check if admins contains a list of valid email addresses
-  validation {
-    condition     = length([for email in var.hana_cloud_admins : can(regex("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$", email))]) == length(var.hana_cloud_admins)
-    error_message = "Please enter a valid email address for the admins of SAP HANA Cloud instance."
-  }
+variable "hana_system_admin" {
+  type        = string
+  description = "Defines the colleague who is added as an admin to access the instance of SAP HANA Cloud."
 }
 
 
@@ -224,56 +164,67 @@ variable "service_plan__sap_process_automation" {
 variable "process_automation_admins" {
   type        = list(string)
   description = "SAP Build Process Automation Administrators"
-  default     = ["jane.doe@test.com", "john.doe@test.com"]
-  # add validation to check if SAP Build Process Automation Administrators contains a list of valid email addresses
-  validation {
-    condition     = length([for email in var.process_automation_admins : can(regex("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$", email))]) == length(var.process_automation_admins)
-    error_message = "Please enter a valid email address for the SAP Build Process Automation Administrators."
-  }
 }
 
 variable "process_automation_developers" {
   type        = list(string)
   description = "SAP Build Process Automation Developers"
-  default     = ["jane.doe@test.com", "john.doe@test.com"]
-  # add validation to check if SAP Build Process Automation Developers contains a list of valid email addresses
-  validation {
-    condition     = length([for email in var.process_automation_developers : can(regex("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$", email))]) == length(var.process_automation_developers)
-    error_message = "Please enter a valid email address for the SAP Build Process Automation Developers."
-  }
 }
 
 variable "process_automation_participants" {
   type        = list(string)
   description = "SAP Build Process Automation Participants"
-  default     = ["jane.doe@test.com", "john.doe@test.com"]
-  # add validation to check if SAP Build Process Automation Participants contains a list of valid email addresses
-  validation {
-    condition     = length([for email in var.process_automation_participants : can(regex("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$", email))]) == length(var.process_automation_participants)
-    error_message = "Please enter a valid email address for the SAP Build Process Automation Participants."
-  }
 }
 
 variable "event_mesh_admins" {
   type        = list(string)
   description = "Enterprise Messaging Administrators"
-  default     = ["jane.doe@test.com", "john.doe@test.com"]
-  # add validation to check if admins contains a list of valid email addresses
-  validation {
-    condition     = length([for email in var.event_mesh_admins : can(regex("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$", email))]) == length(var.event_mesh_admins)
-    error_message = "Please enter a valid email address for the Enterprise Messaging Administrators."
-  }
 }
 
 variable "event_mesh_developers" {
   type        = list(string)
   description = "Enterprise Messaging Developers"
-  default     = ["jane.doe@test.com", "john.doe@test.com"]
-  # add validation to check if Enterprise Messaging Developers contains a list of valid email addresses
+}
+
+# ------------------------------------------------------------------------------------------------------
+# Switch for creating tfvars for step 2
+# ------------------------------------------------------------------------------------------------------
+variable "create_tfvars_file_for_step2" {
+  type        = bool
+  description = "Switch to enable the creation of the tfvars file for step 2."
+  default     = true
+}
+
+variable "cf_space_name" {
+  type        = string
+  description = "Name of the Cloud Foundry space."
+  default     = "dev"
+
   validation {
-    condition     = length([for email in var.event_mesh_developers : can(regex("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$", email))]) == length(var.event_mesh_developers)
-    error_message = "Please enter a valid email address for the Enterprise Messaging Developers."
+    condition     = can(regex("^.{1,255}$", var.cf_space_name))
+    error_message = "The Cloud Foundry space name must not be emtpy and not exceed 255 characters."
   }
 }
+
+variable "cf_org_admins" {
+  type        = list(string)
+  description = "List of users to set as Cloudfoundry org administrators."
+}
+
+variable "cf_org_users" {
+  type        = list(string)
+  description = "List of users to set as Cloudfoundry org users."
+}
+
+variable "cf_space_managers" {
+  type        = list(string)
+  description = "Defines the colleagues who are added to a CF space as space manager."
+}
+
+variable "cf_space_developers" {
+  type        = list(string)
+  description = "Defines the colleagues who are added to a CF space as space developer."
+}
+
 
 
