317c4419f2d7767aeadf1dca1952f452ad87237c
prajin-op <prajin.op@gmail.com>
2025-01-17 19:13:42 +0530

    chore: Update solution for SIT

--- released/SAP-Inside-Tracks/SITBLR_DEC_2024/solution/main.tf
+++ released/SAP-Inside-Tracks/SITBLR_DEC_2024/solution/main.tf
@@ -3,7 +3,6 @@ locals {
   project_subaccount_domain = lower(replace("${var.org_name}-${var.project_name}-${var.stage}", " ", ""))
   project_subaccount_cf_org = replace("${var.org_name}_${lower(var.project_name)}-${lower(var.stage)}", " ", "_")
 }
-
 resource "btp_subaccount" "project" {
   name      = local.project_subaccount_name
   subdomain = local.project_subaccount_domain
@@ -13,48 +12,41 @@ resource "btp_subaccount" "project" {
     "costcenter" = ["${var.costcenter}"]
   }
 }
-
-resource "btp_subaccount_role_collection_assignment" "subaccount_users" {
-  for_each             = toset(var.emergency_admins)
-  subaccount_id        = btp_subaccount.project.id
-  role_collection_name = "Subaccount Administrator"
-  user_name            = each.value
-}
-
 resource "btp_subaccount_entitlement" "bas" {
   subaccount_id = btp_subaccount.project.id
-  service_name  = "sapappstudiotrial"
-  plan_name     = var.bas_plan_name
+  service_name  = var.bas_service_name
+  plan_name     = var.bas_plan
 }
-resource "btp_subaccount_subscription" "bas-subscribe" {
+
+resource "btp_subaccount_subscription" "bas" {
   subaccount_id = btp_subaccount.project.id
-  app_name      = "sapappstudiotrial"
-  plan_name     = var.bas_plan_name
+  app_name      = var.bas_service_name
+  plan_name     = var.bas_plan
   depends_on    = [btp_subaccount_entitlement.bas]
 }
-resource "btp_subaccount_role_collection_assignment" "Business_Application_Studio_Administrator" {
+
+resource "btp_subaccount_role_collection_assignment" "bas_admin" {
   for_each             = toset(var.bas_admins)
   subaccount_id        = btp_subaccount.project.id
   role_collection_name = "Business_Application_Studio_Administrator"
   user_name            = each.value
-  depends_on           = [btp_subaccount_subscription.bas-subscribe]
+  depends_on           = [btp_subaccount_subscription.bas]
 }
 
-resource "btp_subaccount_role_collection_assignment" "Business_Application_Studio_Developer" {
+resource "btp_subaccount_role_collection_assignment" "bas_developer" {
+  for_each             = toset(var.bas_developers)
   subaccount_id        = btp_subaccount.project.id
   role_collection_name = "Business_Application_Studio_Developer"
-  for_each             = toset(var.bas_developers)
   user_name            = each.value
-  depends_on           = [btp_subaccount_subscription.bas-subscribe]
+  depends_on           = [btp_subaccount_subscription.bas]
 }
-
 resource "btp_subaccount_environment_instance" "cloudfoundry" {
   subaccount_id    = btp_subaccount.project.id
   name             = local.project_subaccount_cf_org
   landscape_label  = var.cf_landscape_label
   environment_type = "cloudfoundry"
   service_name     = "cloudfoundry"
-  plan_name        = "trial"
+  plan_name        = var.cf_plan
   parameters = jsonencode({
     instance_name = local.project_subaccount_cf_org
   })
@@ -64,7 +56,6 @@ resource "btp_subaccount_environment_instance" "cloudfoundry" {
     delete = "30m"
   }
 }
-
 resource "cloudfoundry_org_role" "my_role" {
   for_each = var.cf_org_user
   username = each.value
--- released/SAP-Inside-Tracks/SITBLR_DEC_2024/solution/variables.tf
+++ released/SAP-Inside-Tracks/SITBLR_DEC_2024/solution/variables.tf
@@ -3,23 +3,26 @@ variable "globalaccount" {
   description = "The subdomain of the SAP BTP global account."
 }
 
+variable "idp" {
+  type        = string
+  description = "Orgin key of Identity Provider"
+  default     = null
+}
 variable "region" {
   type        = string
   description = "The region where the project account shall be created in."
   default     = "us10"
 }
-
 variable "project_name" {
   type        = string
   description = "The subaccount name."
-  default     = "myproj-1234"
+  default     = "proj-1234"
 
   validation {
     condition     = can(regex("^[a-zA-Z0-9_\\-]{1,200}", var.project_name))
     error_message = "Provide a valid project name."
   }
 }
-
 variable "stage" {
   type        = string
   description = "The stage/tier the account will be used for."
@@ -30,7 +33,6 @@ variable "stage" {
     error_message = "Select a valid stage for the project account."
   }
 }
-
 variable "costcenter" {
   type        = string
   description = "The cost center the account will be billed to."
@@ -41,47 +43,46 @@ variable "costcenter" {
     error_message = "Provide a valid cost center."
   }
 }
-
-variable "bas_plan_name" {
-  description = "BAS plan"
-  type        = string
-  default     = "free"
-}
-
-variable "bas_developers" {
-  description = "BAS developers"
-  type        = list(string)
-  default     = ["jane.doe@test.com", "john.doe@test.com"]
-}
-
-variable "bas_admins" {
-  description = "BAS Admininstrators"
-  type        = list(string)
-  default     = ["jane.doe@test.com", "john.doe@test.com"]
-}
-
 variable "org_name" {
   type        = string
-  description = "Defines to which organisation the project account shall belong to."
+  description = "Defines to which organization the project account shall belong to."
   default     = "B2C"
 }
-
-variable "emergency_admins" {
+variable "bas_admins" {
   type        = list(string)
-  description = "Defines the colleagues who are added to each subaccount as emergency administrators."
-  default     = ["jane.doe@test.com", "john.doe@test.com"]
-}
+  description = "List of users to assign the Administrator role."
 
-variable "cf_space_name" {
+}
+variable "bas_developers" {
+  type        = list(string)
+  description = "List of users to assign the Developer role."
+}
+variable "bas_service_name" {
   type        = string
-  description = "The name of the Cloud Foundry space."
-  default     = "dev"
+  description = "Service name for Business Application Studio."
+  default     = "sapappstudio"
+
+}
+variable "bas_plan" {
+  type        = string
+  description = "Plan name for Business Application Studio."
+  default     = "standard-edition"
 }
 
 variable "cf_landscape_label" {
   type        = string
   description = "The region where the project account shall be created in."
   default     = "cf-us10-001"
+}
+variable "cf_plan" {
+  type        = string
+  description = "Plan name for Cloud Foundry Runtime."
+  default     = "standard"
+}
+variable "cf_space_name" {
+  type        = string
+  description = "The name of the Cloud Foundry space."
+  default     = "dev"
 }
 
 variable "cf_org_user" {
