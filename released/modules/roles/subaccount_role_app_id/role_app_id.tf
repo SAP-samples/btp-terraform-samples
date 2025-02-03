139e225df38eefec6a372664caa20ff21f0b25af
Christian Lechner <lechnerc77@users.noreply.github.com>
2025-02-03 12:51:44 +0100

    feat: add module for fetching app ID for a role (#389)

---
+++ released/modules/roles/subaccount_role_app_id/role_app_id.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    btp = {
+      source  = "SAP/btp"
+      version = "~> 1.9.0"
+    }
+  }
+}
+
+data "btp_subaccount_roles" "all_roles" {
+  subaccount_id = var.subaccount_id
+}
+
+
+locals {
+  selected_role = [
+    for role in data.btp_subaccount_roles.all_roles.values : role
+    if role.name == var.role_name && role.role_template_name == var.role_template_name
+  ]
+}
